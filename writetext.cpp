8decc054c5ed8d3bf0722ac275bec409191302df
dearlord902 <dearlord902@users.noreply.github.com>
2017-04-03 19:06:11 -0400

    Lab 9

--- writetext.cpp
+++ writetext.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main( )
@@ -7,7 +8,16 @@ int main( )
    short int a = -6730;
    float b = 68.123; 
    char c = 'J';
- 
+
+	ofstream test{"abc.txt"};
+	
+	if (test)
+		test <<a<<" "<<b<<" "<<c<<endl;
+	else
+		cout<<"Error abc.txt didn't open"<<endl;
+		
+	test.close();
+	 
    cout<<a<<" "<<b<<" "<<c<<endl;//display data to console (i.e. as formatted chars)
 
    cout<<endl;
